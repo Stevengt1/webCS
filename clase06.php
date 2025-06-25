28fafc98dc4804021f7f41d40ddb350e106f7fc1
Stevn <165967402+Stevengt1@users.noreply.github.com>
2025-06-25 17:49:14 -0600

    Commit mi primer php

---
+++ clase06.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi primer PHP</title>
+    <link rel="stylesheet" href="./Styles/style2.css">
+</head>
+<body>
+    <?php
+    // Agregar un comentario en PHP
+    // Manejo de variables
+    $nombre = "Carlos";
+    $edad = 30;
+    $nota = 85.5;
+    $aprobado = true;
+
+    // Imprimir variables
+    echo "<h1>Hola, $nombre</h1>";
+    print("Tu edad es: $edad años<br>");
+    ?>
+</body>
+</html>
